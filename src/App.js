da8e4db9b04b8f79cc7abebb38bb914c11011cfb
HrishikeshShinde3 <rushishinde0303@gmail.com>
2023-07-17 12:47:18 +0530

    constructor with code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//make class component
+//make constructor in class component
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    //state make before html by using this.state
+    this.state={
+      data:'Hrishi'
+
+    }
+    console.warn('constructor') // constructor made when class made as soon as
+  }
+  render()
+  {
+    console.warn('render')    // then render works
+    return(
+      <h1>Hello::{this.state.data}</h1>//data return from this.state
+    )
+  }
 }
-
 export default App;
